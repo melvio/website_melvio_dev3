25088a266c60973cf5da9a24d6c2ff5e1f3d4d0a
Melvin <31448155+melvio@users.noreply.github.com>
2021-04-23 16:16:24 +0200

    refactor script

--- configure_ec2_server.sh
+++ configure_ec2_server.sh
@@ -4,30 +4,36 @@ set -e
 
 sudo apt -y update
 sudo apt -y upgrade
-sudo apt -y install nginx-full make
-
-sudo apt-get -y remove docker docker-engine docker.io containerd runc
-sudo apt-get -y install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
-sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-
-sudo systemctl start nginx
-
-sudo cp ./nginx/nginx.conf /etc/nginx/nginx.conf
-
-sudo nginx -s reload
+sudo apt -y install make
 
 
+install_docker() {
+  sudo apt-get -y remove docker docker-engine docker.io containerd runc
+  sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+  echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >>/dev/null
+
+  sudo apt-get -y update
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+}
+
+install_docker
+
+run_nginx() {
+  sudo apt -y install nginx-full
+
+  sudo systemctl start nginx
+
+  sudo cp ./nginx/nginx.conf /etc/nginx/nginx.conf
+
+  sudo nginx -s reload
+}
